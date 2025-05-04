0930e5d58fab586547460906c630925c1a8d8164
Murugavel-CDW <murugavel.v@cdw.com>
2025-05-05 00:27:08 +0530

    fix: removal of cart list

--- Murugavel/sitback-furniture/src/containers/cart-container/CartContainer.jsx
+++ Murugavel/sitback-furniture/src/containers/cart-container/CartContainer.jsx
@@ -1,11 +1,15 @@
-import CartList from '../../components/cart-list/CartList';
+import CartItemCard from '../../components/cart-item-card/CartItemCard';
+import { useCartContext } from '../../context/CartContext';
 import './cart-container.css'
 
 const CartContainer = () => {
+    const {cartData} = useCartContext();
     return (
         <div className="cart-container">
             <h2 className="cart-text">MY CART</h2>
-            <CartList />
+            <div className='cart-item-list'>
+                {cartData.map((orderData) => <CartItemCard productData={orderData} />)}
+            </div>
         </div>
     )
 }
